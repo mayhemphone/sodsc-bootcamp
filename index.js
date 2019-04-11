2346e5cbd204dd1360bce2f0a2685368987361be
mayhemphone <mayhemphone@gmail.com>
2019-04-11 11:33:42 -0700

    initialize

---
+++ index.js
@@ -0,0 +1,13 @@
+// Require necessary modules
+
+// Declare Express app
+
+// Set view engine
+
+// Include (use) middle ware
+
+// Include routes from controllers
+
+// make home route
+
+//	listen
