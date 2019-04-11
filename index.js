98193ded5d09729004a78a544426cda9451f944f
mayhemphone <mayhemphone@gmail.com>
2019-04-11 11:47:47 -0700

    home page stubbed

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+// Include .env variables
+require('dotenv').config()
+
 // Require necessary modules
+let express = require('express')
 
 // Declare Express app
+let app = express()
 
 // Set view engine
+app.set('view engine', 'ejs')
 
 // Include (use) middle ware
 
+
 // Include routes from controllers
 
-// make home route
 
-//	listen
+// make home route
+app.get('/', (req, res)=>{
+	res.send('STUB')
+})
+//	listen from your port
+app.listen(process.env.PORT || 3000)
