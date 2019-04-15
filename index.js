b593af8b111a40f88c7ab3fb17bb1ba5332f105e
mayhemphone <mayhemphone@gmail.com>
2019-04-15 12:08:15 -0700

    added alerts and flash

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Include .env variables
 require('dotenv').config()
 
+
 // Require necessary modules
 let express = require('express')
 let flash = require('connect-flash')
 let layouts = require('express-ejs-layouts')
+let session = require('express-session')
 
 // Declare Express app
 let app = express()
@@ -14,8 +16,20 @@ app.set('view engine', 'ejs')
 
 // Include (use) middle ware
 app.use(layouts)
-app.use(flash())
+
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: true
+}))
+app.use(flash())
+
+//custom middleware - write data to locals
+app.use((req, res, next) =>{
+	res.locals.alerts = req.flash()
+	next()
+})
 
 // Include routes from controllers
 app.use('/auth', require('./controllers/auth'))
